f2a513d3d15718a528686dc707161de65292ad1d
waningflow <jack1995woods@gmail.com>
2020-09-20 21:43:33 +0800

    feat: update game

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -8,44 +8,171 @@ class Game extends React.Component {
   }
 
   componentDidMount() {
-    var canvas = document.getElementById('game-canvas');
-    // Load the 3D engine
-    var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
-    // CreateScene function that creates and return the scene
+    var canvas = document.getElementById('game-canvas'); // Get the canvas element
+    var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+    /******* Add the create scene function ******/
     var createScene = function () {
-      // Create a basic BJS Scene object
+      // Create the scene space
       var scene = new BABYLON.Scene(engine);
-      // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-      var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
-      // Target the camera to scene origin
-      camera.setTarget(BABYLON.Vector3.Zero());
-      // Attach the camera to the canvas
-      camera.attachControl(canvas, false);
-      // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
-      var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-      // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-      var sphere = BABYLON.Mesh.CreateSphere(
-        'sphere1',
-        16,
-        2,
-        scene,
-        false,
-        BABYLON.Mesh.FRONTSIDE
+
+      // Add a camera to the scene and attach it to the canvas
+      var camera = new BABYLON.ArcRotateCamera(
+        'Camera',
+        -Math.PI / 2,
+        Math.PI / 4,
+        6,
+        BABYLON.Vector3.Zero(),
+        scene
       );
-      // Move the sphere upward 1/2 of its height
-      sphere.position.y = 1;
-      // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-      var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
-      // Return the created scene
+      camera.attachControl(canvas, true);
+      camera.minZ = 0.1;
+
+      // Initialize GizmoManager
+      var gizmoManager = new BABYLON.GizmoManager(scene);
+
+      // Initialize all gizmos
+      gizmoManager.boundingBoxGizmoEnabled = true;
+      gizmoManager.positionGizmoEnabled = true;
+      gizmoManager.rotationGizmoEnabled = true;
+      gizmoManager.scaleGizmoEnabled = true;
+
+      // Modify gizmos based on keypress
+      document.onkeydown = (e) => {
+        if (e.key == 'w' || e.key == 'e' || e.key == 'r' || e.key == 'q') {
+          // Switch gizmo type
+          gizmoManager.positionGizmoEnabled = false;
+          gizmoManager.rotationGizmoEnabled = false;
+          gizmoManager.scaleGizmoEnabled = false;
+          gizmoManager.boundingBoxGizmoEnabled = false;
+          if (e.key == 'w') {
+            gizmoManager.positionGizmoEnabled = true;
+          }
+          if (e.key == 'e') {
+            gizmoManager.rotationGizmoEnabled = true;
+          }
+          if (e.key == 'r') {
+            gizmoManager.scaleGizmoEnabled = true;
+          }
+          if (e.key == 'q') {
+            gizmoManager.boundingBoxGizmoEnabled = true;
+          }
+        }
+        if (e.key == 'y') {
+          // hide the gizmo
+          gizmoManager.attachToMesh(null);
+        }
+        if (e.key == 'a') {
+          // Toggle local/global gizmo rotation positioning
+          gizmoManager.gizmos.positionGizmo.updateGizmoRotationToMatchAttachedMesh = !gizmoManager
+            .gizmos.positionGizmo.updateGizmoRotationToMatchAttachedMesh;
+          gizmoManager.gizmos.rotationGizmo.updateGizmoRotationToMatchAttachedMesh = !gizmoManager
+            .gizmos.rotationGizmo.updateGizmoRotationToMatchAttachedMesh;
+        }
+        if (e.key == 's') {
+          // Toggle distance snapping
+          if (gizmoManager.gizmos.scaleGizmo.snapDistance == 0) {
+            gizmoManager.gizmos.scaleGizmo.snapDistance = 0.3;
+            gizmoManager.gizmos.rotationGizmo.snapDistance = 0.3;
+            gizmoManager.gizmos.positionGizmo.snapDistance = 0.3;
+          } else {
+            gizmoManager.gizmos.scaleGizmo.snapDistance = 0;
+            gizmoManager.gizmos.rotationGizmo.snapDistance = 0;
+            gizmoManager.gizmos.positionGizmo.snapDistance = 0;
+          }
+        }
+        if (e.key == 'd') {
+          // Toggle gizmo size
+          if (gizmoManager.gizmos.scaleGizmo.scaleRatio == 1) {
+            gizmoManager.gizmos.scaleGizmo.scaleRatio = 1.5;
+            gizmoManager.gizmos.rotationGizmo.scaleRatio = 1.5;
+            gizmoManager.gizmos.positionGizmo.scaleRatio = 1.5;
+          } else {
+            gizmoManager.gizmos.scaleGizmo.scaleRatio = 1;
+            gizmoManager.gizmos.rotationGizmo.scaleRatio = 1;
+            gizmoManager.gizmos.positionGizmo.scaleRatio = 1;
+          }
+        }
+      };
+
+      // Start by only enabling position control
+      document.onkeydown({ key: 'w' });
+
+      // Create objects
+      var hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(
+        'http://texture.waningflow.com/uno/environment.dds',
+        scene
+      );
+      var hdrSkybox = BABYLON.Mesh.CreateBox('hdrSkyBox', 1000.0, scene);
+      hdrSkybox.isPickable = false;
+      var hdrSkyboxMaterial = new BABYLON.PBRMaterial('skyBox', scene);
+      hdrSkyboxMaterial.backFaceCulling = false;
+      hdrSkyboxMaterial.reflectionTexture = hdrTexture.clone();
+      hdrSkyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+      hdrSkyboxMaterial.microSurface = 1.0;
+      hdrSkyboxMaterial.disableLighting = true;
+      hdrSkybox.material = hdrSkyboxMaterial;
+      hdrSkybox.infiniteDistance = true;
+      var sphereGlass = BABYLON.Mesh.CreateSphere('sphereGlass', 48, 1.0, scene);
+      sphereGlass.translate(new BABYLON.Vector3(1, 0, 0), -3);
+      var sphereMetal = BABYLON.Mesh.CreateSphere('sphereMetal', 48, 1.0, scene);
+      sphereMetal.translate(new BABYLON.Vector3(1, 0, 0), 3);
+      var spherePlastic = BABYLON.Mesh.CreateSphere('spherePlastic', 48, 1.0, scene);
+      spherePlastic.translate(new BABYLON.Vector3(0, 0, 1), -3);
+      var woodPlank = BABYLON.MeshBuilder.CreateBox(
+        'plane',
+        { width: 3, height: 0.1, depth: 3 },
+        scene
+      );
+      var glass = new BABYLON.PBRMaterial('glass', scene);
+      glass.reflectionTexture = hdrTexture;
+      glass.refractionTexture = hdrTexture;
+      glass.linkRefractionWithTransparency = true;
+      glass.indexOfRefraction = 0.52;
+      glass.alpha = 0;
+      glass.microSurface = 1;
+      glass.reflectivityColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+      glass.albedoColor = new BABYLON.Color3(0.85, 0.85, 0.85);
+      sphereGlass.material = glass;
+      var metal = new BABYLON.PBRMaterial('metal', scene);
+      metal.reflectionTexture = hdrTexture;
+      metal.microSurface = 0.96;
+      metal.reflectivityColor = new BABYLON.Color3(0.85, 0.85, 0.85);
+      metal.albedoColor = new BABYLON.Color3(0.01, 0.01, 0.01);
+      sphereMetal.material = metal;
+      var plastic = new BABYLON.PBRMaterial('plastic', scene);
+      plastic.reflectionTexture = hdrTexture;
+      plastic.microSurface = 0.96;
+      plastic.albedoColor = new BABYLON.Color3(0.206, 0.94, 1);
+      plastic.reflectivityColor = new BABYLON.Color3(0.003, 0.003, 0.003);
+      spherePlastic.material = plastic;
+      var wood = new BABYLON.PBRMaterial('wood', scene);
+      wood.reflectionTexture = hdrTexture;
+      wood.environmentIntensity = 1;
+      wood.specularIntensity = 0.3;
+      wood.reflectivityTexture = new BABYLON.Texture(
+        'http://texture.waningflow.com/uno/reflectivity.png',
+        scene
+      );
+      wood.useMicroSurfaceFromReflectivityMapAlpha = true;
+      wood.albedoColor = BABYLON.Color3.White();
+      wood.albedoTexture = new BABYLON.Texture(
+        'http://texture.waningflow.com/uno/albedo.png',
+        scene
+      );
+      woodPlank.material = wood;
       return scene;
     };
-    // call the createScene function
-    var scene = createScene();
-    // run the render loop
+    /******* End of the create scene function ******/
+
+    var scene = createScene(); //Call the createScene function
+
+    // Register a render loop to repeatedly render the scene
     engine.runRenderLoop(function () {
       scene.render();
     });
-    // the canvas/window resize event handler
+
+    // Watch for browser/canvas resize events
     window.addEventListener('resize', function () {
       engine.resize();
     });
