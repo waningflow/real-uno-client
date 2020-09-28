68cec1f41e827cccec9091c6ed0365e964e6ecb0
waningflow <jack1995woods@gmail.com>
2020-09-28 23:11:03 +0800

    feat: pick multi card

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -80,46 +80,48 @@ class Game extends React.Component {
       cards[i] = card;
     }
 
-    let topCard = cards[107];
+    function pickCard(currentCard) {
+      let qEase = new BABYLON.QuinticEase();
+      qEase.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEOUT);
 
-    let qEase = new BABYLON.QuinticEase();
-    qEase.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEOUT);
+      let pickCardAnimation = new BABYLON.Animation(
+        'pickCardAnimation',
+        'position',
+        30,
+        BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
+        BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
+      );
+      let keys = [];
+      keys.push({ frame: 0, value: currentCard.position });
+      keys.push({ frame: 100, value: new BABYLON.Vector3(0, 2, -5.5) });
+      pickCardAnimation.setKeys(keys);
+      pickCardAnimation.setEasingFunction(qEase);
 
-    let pickCardAnimation = new BABYLON.Animation(
-      'pickCardAnimation',
-      'position',
-      30,
-      BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
-      BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
-    );
-    let keys = [];
-    keys.push({ frame: 0, value: topCard.position });
-    keys.push({ frame: 100, value: new BABYLON.Vector3(0, 2, -5.5) });
-    pickCardAnimation.setKeys(keys);
-    pickCardAnimation.setEasingFunction(qEase);
+      let pickCardAnimation2 = new BABYLON.Animation(
+        'pickCardAnimation2',
+        'rotation',
+        30,
+        BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
+        BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
+      );
+      let keys2 = [];
+      keys2.push({ frame: 0, value: currentCard.rotation });
+      keys2.push({ frame: 10, value: currentCard.rotation });
+      keys2.push({
+        frame: 100,
+        value: new BABYLON.Vector3(0, Math.PI / 2, Math.PI / 2 + Math.PI / 4),
+      });
+      pickCardAnimation2.setKeys(keys2);
+      pickCardAnimation2.setEasingFunction(qEase);
 
-    let pickCardAnimation2 = new BABYLON.Animation(
-      'pickCardAnimation2',
-      'rotation',
-      30,
-      BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
-      BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
-    );
-    let keys2 = [];
-    keys2.push({ frame: 0, value: topCard.rotation });
-    keys2.push({ frame: 10, value: topCard.rotation });
-    keys2.push({
-      frame: 100,
-      value: new BABYLON.Vector3(0, Math.PI / 2, Math.PI / 2 + Math.PI / 4),
-    });
-    pickCardAnimation2.setKeys(keys2);
-    pickCardAnimation2.setEasingFunction(qEase);
-
-    topCard.animations.push(pickCardAnimation);
-    topCard.animations.push(pickCardAnimation2);
+      currentCard.animations.push(pickCardAnimation);
+      currentCard.animations.push(pickCardAnimation2);
+      return scene.beginAnimation(currentCard, 0, 100, false, 3);
+    }
     setTimeout(async () => {
-      var anim = scene.beginAnimation(topCard, 0, 100, false, 3);
-      // await anim.waitAsync();
+      for (let i = 107; i >= 0; i--) {
+        await pickCard(cards[i]).waitAsync();
+      }
     }, 1000);
   }
 
